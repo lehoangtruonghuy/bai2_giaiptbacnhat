1ea9b9d76f28f7a3f0bb9ce6ced8fddf7762b3aa
huyle <huyle@DESKTOP-UU790CD>
2025-02-23 01:21:51 +0700

    Add project files.

---
+++ bai2_giaiptbacnhat/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        int intA, intB;
+        double X;
+        Console.WriteLine(" Chuong Trinh Giai Phuong Trinh Bac 1 ");
+        Console.WriteLine(" aX + b = 0 ");
+        Console.Write(" Nhap a: ");
+        intA = Convert.ToInt32(Console.ReadLine());
+        Console.Write(" {0}", "Nhap b: ");
+        intB = Convert.ToInt32(Console.ReadLine());
+        if (intA == 0 && intB == 0)
+        {
+            Console.WriteLine(" Phuong Trinh Co Vo So Nghiem ");
+        }
+        else if (intA == 0 && intB != 0)
+        {
+            Console.WriteLine(" Phuong Trinh Vo Nghiem ");
+        }
+        else
+        {
+            X = (double)-intB / intA;
+            Console.WriteLine(" Phuong Trinh Co 1 Nghiem Duy Nhat x = {0} ", X);
+        }
+    }
+}
